94035135929bba1ab872e4df4867ea7482755af8
Caden Buckhalt <halaeb@gmail.com>
2023-09-06 12:34:17 -0700

    bring over scripts, install prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
       <h1 className="text-4xl font-bold text-center">
         Fresco Initial Setup Flow Test
       </h1>
+      <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fbuckhalt%2Ffresco-setup-test.git&env=NEXTAUTH_SECRET,NEXTAUTH_URL,UPLOADTHING_SECRET,UPLOADTHING_APP_ID,DATABASE_URL&envDescription=Environment%20Variables%20needed%20for%20the%20application&envLink=https%3A%2F%2Fdocumentation.networkcanvas.com%2F">
+        <img src="https://vercel.com/button" alt="Deploy with Vercel" />
+      </a>
     </main>
   );
 }
