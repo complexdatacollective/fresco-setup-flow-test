66f3bd7ee18a69b937756bdbc050738a68a04c37
Caden Buckhalt <halaeb@gmail.com>
2023-09-07 14:34:12 -0700

    fix repo name since moved

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -17,7 +17,7 @@ export default function Setup() {
           create an account within five minutes of deploying.
         </p>
       )}
-      <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fbuckhalt%2Ffresco-setup-test&env=DATABASE_URL,NEXTAUTH_SECRET,NEXTAUTH_URL,UPLOADTHING_SECRET,UPLOADTHING_APP_ID&envDescription=Environment%20variables%20needed%20for%20configuring%20Fresco.&envLink=https%3A%2F%2Fdocumentation.networkcanvas.com%2F&project-name=fresco&repository-name=fresco&redirect-url=https%3A%2F%2Fdocumentation.networkcanvas.com%2F&demo-title=Fresco&demo-description=Deploy%20Network%20Canvas%20interviews%20in%20the%20web%20browser.&demo-url=https%3A%2F%2Fnetworkcanvas.com%2F&demo-image=https%3A%2F%2Fframerusercontent.com%2Fimages%2FUvNFzJTlj8T2Tn8zWSNGpnrkOI.svg">
+      <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fcomplexdatacollective%2Ffresco-setup-test&env=DATABASE_URL,NEXTAUTH_SECRET,NEXTAUTH_URL,UPLOADTHING_SECRET,UPLOADTHING_APP_ID&envDescription=Environment%20variables%20needed%20for%20configuring%20Fresco.&envLink=https%3A%2F%2Fdocumentation.networkcanvas.com%2F&project-name=fresco&repository-name=fresco&redirect-url=https%3A%2F%2Fdocumentation.networkcanvas.com%2F&demo-title=Fresco&demo-description=Deploy%20Network%20Canvas%20interviews%20in%20the%20web%20browser.&demo-url=https%3A%2F%2Fnetworkcanvas.com%2F&demo-image=https%3A%2F%2Fframerusercontent.com%2Fimages%2FUvNFzJTlj8T2Tn8zWSNGpnrkOI.svg">
         <img src="https://vercel.com/button" alt="Deploy with Vercel" />
       </a>
     </div>
