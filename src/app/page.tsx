822c01e05b31e8778b09dd9cabd9d4eb9a3477a7
Caden Buckhalt <halaeb@gmail.com>
2023-09-07 10:30:50 -0700

    update deploy link, install zod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         Fresco Initial Setup Flow Test
       </h1>
       <div>{status ? <p>{status}</p> : <p>Loading...</p>}</div>
-      <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fbuckhalt%2Ffresco-setup-test.git&env=NEXTAUTH_SECRET,NEXTAUTH_URL,UPLOADTHING_SECRET,UPLOADTHING_APP_ID,DATABASE_URL&envDescription=Environment%20Variables%20needed%20for%20the%20application&envLink=https%3A%2F%2Fdocumentation.networkcanvas.com%2F">
+      <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fbuckhalt%2Ffresco-setup-test&env=DATABASE_URL,NEXTAUTH_SECRET,NEXTAUTH_URL&envDescription=Environment%20variables%20needed%20for%20configuring%20Fresco.&envLink=https%3A%2F%2Fdocumentation.networkcanvas.com%2F&project-name=fresco&repository-name=fresco&redirect-url=https%3A%2F%2Fdocumentation.networkcanvas.com%2F&demo-title=Fresco&demo-description=Deploy%20Network%20Canvas%20interviews%20in%20the%20web%20browser.&demo-url=https%3A%2F%2Fnetworkcanvas.com%2F&demo-image=https%3A%2F%2Fframerusercontent.com%2Fimages%2FUvNFzJTlj8T2Tn8zWSNGpnrkOI.svg">
         <img src="https://vercel.com/button" alt="Deploy with Vercel" />
       </a>
     </main>
